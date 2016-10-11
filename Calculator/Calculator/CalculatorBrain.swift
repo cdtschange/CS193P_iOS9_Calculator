60f0dde25ce2343bf097ce82db94090322179a9d
wei.mao <wei.mao@jimubox.com>
2016-10-11 09:46:37 +0800

    添加program

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -11,6 +11,7 @@ import Foundation
 class CalculatorBrain {
     
     private var accumulator = 0.0
+    private var internalProgram = [AnyObject]()
     
     func setOperand(operand: Double) {
         accumulator = operand
@@ -62,6 +63,32 @@ class CalculatorBrain {
     private struct PendingBinaryOperationInfo {
         var binaryFunction: (Double, Double) -> Double
         var firstOperand: Double
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand)
+                    } else if let operation = op as? String {
+                        performOperation(operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
     }
     
     var result: Double {
